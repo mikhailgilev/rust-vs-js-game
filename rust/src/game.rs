ca082b1c0536ff5f872e12f04300aaf65b497bf2
Mikhail Gilev <gilev820@gmail.com>
2023-07-21 17:15:03 +0500

    minimal compiling implementation of state machine

--- rust/src/game.rs
+++ rust/src/game.rs
@@ -7,6 +7,8 @@ use web_sys::HtmlImageElement;
 mod red_hat_boy_states {
     use crate::engine::Point;
     const FLOOR: i16 = 475;
+    const IDLE_FRAME_NAME: &str = "Idle";
+    const RUN_FRAME_NAME: &str = "Run";
 
     #[derive(Copy, Clone)]
     pub struct RedHatBoyState<S> {
@@ -16,15 +18,21 @@ mod red_hat_boy_states {
 
     #[derive(Copy, Clone)]
     pub struct RedHatBoyContext {
-        frame: u8,
-        position: Point,
-        velocity: Point,
+        pub frame: u8,
+        pub position: Point,
+        pub velocity: Point,
     }
 
     #[derive(Copy, Clone)]
     pub struct Idle;
     #[derive(Copy, Clone)]
     pub struct Running;
+
+    impl<S> RedHatBoyState<S> {
+        pub fn context(&self) -> &RedHatBoyContext {
+            &self.context
+        }
+    }
 
     impl RedHatBoyState<Idle> {
         pub fn run(self) -> RedHatBoyState<Running> {
@@ -42,6 +50,16 @@ mod red_hat_boy_states {
                 },
                 _state: Idle {},
             }
+        }
+
+        pub fn frame_name(&self) -> &str {
+            IDLE_FRAME_NAME
+        }
+    }
+
+    impl RedHatBoyState<Running> {
+        pub fn frame_name(&self) -> &str {
+            RUN_FRAME_NAME
         }
     }
 }
@@ -63,6 +81,20 @@ impl RedHatBoyStateMachine {
         match (self, event) {
             (RedHatBoyStateMachine::Idle(state), Event::Run) => state.run().into(),
             _ => self,
+        }
+    }
+
+    fn frame_name(&self) -> &str {
+        match self {
+            RedHatBoyStateMachine::Idle(state) => state.frame_name(),
+            RedHatBoyStateMachine::Running(state) => state.frame_name(),
+        }
+    }
+
+    fn context(&self) -> &RedHatBoyContext {
+        match self {
+            RedHatBoyStateMachine::Idle(state) => &state.context(),
+            RedHatBoyStateMachine::Running(state) => &state.context(),
         }
     }
 }
@@ -94,7 +126,11 @@ impl RedHatBoy {
             self.state_machine.frame_name(),
             (self.state_machine.context().frame / 3) + 1
         );
-        let sprite = self.sheet.frames.get(&frame_name).expect("Cell not found");
+        let sprite = self
+            .sprite_sheet
+            .frames
+            .get(&frame_name)
+            .expect("Cell not found");
         renderer.draw_image(
             &self.image,
             &Rect {
@@ -205,5 +241,6 @@ impl Game for WalkTheDog {
                 },
             );
         });
+        self.rhb.as_ref().unwrap().draw(renderer);
     }
 }
