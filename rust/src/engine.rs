f36f262065cc5ef0470479aed7f9ffc4be2451cb
Mikhail Gilev <gilev820@gmail.com>
2023-08-02 10:12:03 +0500

    added restart game button

--- rust/src/engine.rs
+++ rust/src/engine.rs
@@ -12,6 +12,7 @@ use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use web_sys::AudioBuffer;
 use web_sys::AudioContext;
+use web_sys::HtmlElement;
 use web_sys::{CanvasRenderingContext2d, HtmlImageElement};
 
 use crate::browser;
@@ -359,4 +360,14 @@ impl Audio {
 #[derive(Clone)]
 pub struct Sound {
     buffer: AudioBuffer,
+}
+
+pub fn add_click_handler(elem: HtmlElement) -> UnboundedReceiver<()> {
+    let (mut click_sender, click_receiver) = unbounded();
+    let on_click = browser::closure_wrap(Box::new(move || {
+        click_sender.start_send(());
+    }) as Box<dyn FnMut()>);
+    elem.set_onclick(Some(on_click.as_ref().unchecked_ref()));
+    on_click.forget();
+    click_receiver
 }
